088024716bcd2fd1a936741e48de4e2e3483e8e9
wendy <enyinnapc16127@gmail.com>
2022-07-28 23:31:58 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -9,21 +9,44 @@ let resultArea = document.querySelector('.resultArea');
 
 let redactBtn = document.querySelector('.redactBtn');
 
+let timeResult = document.querySelector('.time');
+let scannedword = document.querySelector('.scannedWord')
 
 
-function redacTr(userInput, wordToScramble, newWord){
+
+function redacTr(userInput, wordToScramble, replacerWord){
+ const start = performance.now()
     userInput = (inputText.value);
     wordToScramble = scrambleWord.value
-    newWord = (replaceWord.value) || "***";
+    let scannedWordCount = userInput.split(' ').length
+    replacerWord = (replaceWord.value) ;
+
+    wordToScramble = wordToScramble.split(', ');
+
+    for(let i =0; i < wordToScramble.length; i++){
+      let match = new RegExp(`(${wordToScramble[i]})`, 'gi')
+      userInput = userInput.replaceAll(match, replacerWord ? replacerWord : hideWord)
     
+      resultArea.textContent = userInput;
+
+
     
-    let wordInToArray = userInput.split(/\s/)
-    wordInToArray = wordInToArray.map((item) => {
-      return item == wordToScramble ? newWord : item ;
-    }) 
-    resultArea.textContent = (wordInToArray.join(' '))
+    }
+
+    function hideWord(wordToScramble){
+      if(wordToScramble.length > 0){
+        return wordToScramble.substring(0,0) + "*".repeat(wordToScramble.length )
+      }
+      
+    }
+    
+   const time = (performance.now() - start)
+  
     redactBtn.addEventListener('click', () => {
-        resultSec.style.display = 'block'
+      resultSec.style.display = 'block'
+      timeResult.textContent = `Time: ${time}ms`
+      scannedword.textContent = `ScrambledWordCount: ${scannedWordCount}`
+      
     })
     
  }
